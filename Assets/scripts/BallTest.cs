b61b0b0f3e6da2481f815ee910336fcdcbd46263
toshi184439 <toshihide184439@gmail.com>
2022-08-18 20:37:32 +0900

    [fix] change the method of speed count

--- Assets/scripts/BallTest.cs
+++ Assets/scripts/BallTest.cs
@@ -9,11 +9,13 @@ public class BallTest : MonoBehaviour
     public Rigidbody _rb;
     public int power;
     private UnityEvent<string> DisplaySpeedEvent = new UnityEvent<string>();
-    private Vector3 firstDirection = new Vector3(0f, 0.01f, -1.0f); //カメラの方向に投げてみる
+    private Vector3 firstDirection = new Vector3(0f, 0.05f, -2.0f); //カメラの方向に投げてみる
     private Vector3 startPosition;
     private Quaternion startRotation;
     private GameObject ballObject;
     private float startTime;
+    private int speedcount;
+    private float speedsum;
 
     // Start is called before the first frame update
     void Start() {
@@ -28,15 +30,25 @@ public class BallTest : MonoBehaviour
     // Update is called once per frame
     void Update() {
         Vector3 currentPosition = ballObject.transform.position;
-        if (currentPosition.z < -0.5 && (Time.time - startTime ) > 2.0f ) {
-            double speedText = (_rb.velocity.magnitude * 3.6);
-            DisplaySpeedEvent?.Invoke("ball speed\n" + speedText.ToString() + " km/h");
+        if (currentPosition.z < -0.5 ) {
+          if (speedcount < 1) {
+            speedcount++;
+            speedsum += _rb.velocity.z;
+          }
+
+          if ((Time.time - startTime ) > 2.0f ) {
+            float speedText = ((speedsum / speedcount) * -3.6f);
+            speedText = Mathf.Floor(speedText);
+            DisplaySpeedEvent?.Invoke(speedText.ToString() + "km/h");
             _rb.velocity = Vector3.zero;
             _rb.angularVelocity = Vector3.zero;
             ballObject.transform.position = startPosition;
             ballObject.transform.rotation = startRotation;
             _rb.AddForce(firstDirection * power);
             startTime = Time.time;
+            speedcount = 0;
+            speedsum = 0.0f;
+           }
         }
     }
 }
