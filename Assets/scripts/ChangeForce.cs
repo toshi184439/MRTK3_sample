c0d82021e526955bdb74447fe5112449812df033
toshi184439 <toshihide184439@gmail.com>
2022-09-05 00:03:54 +0900

    [update] update changeForce for variable type of bracking ball

--- Assets/scripts/ChangeForce.cs
+++ Assets/scripts/ChangeForce.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class ChangeForce : MonoBehaviour
 {
+    public enum TypeOfBreakingBall : int {
+      straight = 0,
+      actual = 1
+    }
+
+    private ChangeForce() {}
+
+    ChangeForce instance;
 
     int _changePower = 30; //変化させる力
 
-    Vector3 _shootDirection = new Vector3(-1.0f, -1.0f, 0);
-    Vector3 _sliderDirection = new Vector3(1.0f, -0.1f, 0);
+    public TypeOfBreakingBall _TypeOfBreakingBall = TypeOfBreakingBall.actual;
+
+    Vector3 _shootDirection = new Vector3(-1.0f, -0.1f, 0);
+    Vector3 _sinkerDirection = new Vector3(-0.5f, -0.5f, 0);
     Vector3 _forkDirection = new Vector3(0.0f, -1.0f, 0);
-    Vector3 _straightDirection = Vector3.zero;
+    Vector3 _curveDirection = new Vector3(0.5f, -0.5f, 0);
+    Vector3 _sliderDirection = new Vector3(1.0f, -0.1f, 0);
+
+    string[] _typeofBrakingBall = new string[] {"straight", "shoot", "sinker", "fork", "curve", "slider"};
+
+    List<Vector3> _typeBreackingBallVec = new List<Vector3>();
+
+    void Awake() {
+      if(instance == null) {
+        instance = this;
+      }
+
+      _typeBreackingBallVec.Add(Vector3.zero);
+      _typeBreackingBallVec.Add(_shootDirection);
+      _typeBreackingBallVec.Add(_sinkerDirection);
+      _typeBreackingBallVec.Add(_forkDirection);
+      _typeBreackingBallVec.Add(_curveDirection);
+      _typeBreackingBallVec.Add(_sliderDirection);
+    }
 
     private void OnTriggerEnter(Collider other) {
         Debug.Log("OnTriggerEnter");
         if (other.gameObject.tag == "Ball") {
             Debug.Log("change direction");
             Rigidbody ball_rb = other.gameObject.GetComponent<Rigidbody>();
-            ball_rb.AddForce(_sliderDirection * _changePower);//ボールに力を加える
+            ChangeBallForce(ball_rb);
         }
+    }
+
+    private void ChangeBallForce(Rigidbody ball_rb) {
+      int rnd = Random.Range(0, 6);
+      ball_rb.AddForce(_typeBreackingBallVec[rnd] * _changePower);  //ボールに力を加える
+      Debug.Log(_typeofBrakingBall[rnd]);
     }
 
     // Start is called before the first frame update
